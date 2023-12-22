3923d2d9fa48cab56a82a4a603c8d9f715cb2779
vegabook <thomas@crvm.io>
2023-12-22 21:21:49 +0000

    working tests on fx ticker add

--- lib/blxx/dag/dag.ex
+++ lib/blxx/dag/dag.ex
@@ -5,16 +5,24 @@ defmodule Blxx.Dag do
 
   # -------------- dets vstore ---------------
 
-  def open_store(detspath) when is_binary(detspath) do  
+  def open_store(detspath) when is_binary(detspath) do
     charpath = detspath |> to_charlist
+
     with {:isdir, false} <- {:isdir, detspath |> String.slice(-1, 1) == "/"},
-         {:direxists, true} <- {:direxists, detspath |> Path.dirname |> File.exists?},
-         {:open, {:ok, _}} <- {:open, :dets.open_file(detspath, [{:file, charpath}, {:type, :bag}, {:keypos, 1}, {:repair, true}])} do
-           if :dets.lookup(detspath, :root) == [] do
-             clean_nodes(detspath)
-           else
-             {:ok, {detspath, expand_graph(:digraph.new(), dets_nodes(detspath))}}
-           end
+         {:direxists, true} <- {:direxists, detspath |> Path.dirname() |> File.exists?()},
+         {:open, {:ok, _}} <-
+           {:open,
+            :dets.open_file(detspath, [
+              {:file, charpath},
+              {:type, :bag},
+              {:keypos, 1},
+              {:repair, true}
+            ])} do
+      if :dets.lookup(detspath, :root) == [] do
+        clean_nodes(detspath)
+      else
+        {:ok, {detspath, expand_graph(:digraph.new(), dets_nodes(detspath))}}
+      end
     else
       {:isdir, true} ->
         {:error, "detspath must include a filename"}
@@ -27,33 +35,32 @@ defmodule Blxx.Dag do
     end
   end
 
-  def open_store(detspath) when is_list(detspath) do  
+  def open_store(detspath) when is_list(detspath) do
     # charlist 
     detspath |> to_string |> open_store
   end
-
 
   def close_store(dg) when is_tuple(dg) do
     {detspath, _graph} = dg
     :dets.close(detspath)
   end
 
-
   def close_store(detspath) do
     :dets.close(detspath)
   end
 
-
   def add_vertex({detspath, graph}, v, parent \\ :root, meta \\ %{}, ts \\ Blxx.Util.utc_stamp()) do
     # store a vertex in the vstore and add it to the graph 
-    testgraph = graph # test if adding it works before committing to dets
+    # test if adding it works before committing to dets
+    testgraph = graph
 
     with {:vatom, true} <- {:vatom, is_atom(v)},
          {:patom, true} <- {:patom, is_atom(parent)},
          {:tsnum, true} <- {:tsnum, is_number(ts)},
          {:mmap, true} <- {:mmap, is_map(meta)},
          # no duplicate edge, also handles parent doesn't exist
-         {:novdupe, true} <- {:novdupe, !Enum.member?(:digraph.out_neighbours(testgraph, parent), v)},
+         {:novdupe, true} <-
+           {:novdupe, !Enum.member?(:digraph.out_neighbours(testgraph, parent), v)},
          # don't have to check dupe vertices becausing adding twice changes nothing even edges
          {:addvertex, v} <- {:addvertex, :digraph.add_vertex(testgraph, v, meta)},
          # add edge from parent to v
@@ -91,7 +98,6 @@ defmodule Blxx.Dag do
     end
   end
 
-
   def add_edge({detspath, graph}, parent, child, ts \\ Blxx.Util.utc_stamp()) do
     # store an edge in the vstore and add it to the graph
     # not usually needed as store_vertex always takes a parent but
@@ -99,14 +105,13 @@ defmodule Blxx.Dag do
     testgraph = graph
 
     with {:tsnum, true} <- {:tsnum, is_number(ts)},
-         {:novdupe, true} <- 
+         {:novdupe, true} <-
            {:novdupe, !Enum.member?(:digraph.out_neighbours(testgraph, parent), child)},
          {:addedge, [:"$e" | rest]} <-
            {:addedge, :digraph.add_edge(testgraph, parent, child)},
          {:insert, ok} <-
            {:insert, :dets.insert(detspath, {child, parent, ts, :edge, %{}})} do
-
-          {:ok, {detspath, expand_graph(graph, [{child, parent, ts, :edge, %{}}])}}
+      {:ok, {detspath, expand_graph(graph, [{child, parent, ts, :edge, %{}}])}}
     else
       false -> {:error, "v1 and v2 must be atoms"}
       {:tsnum, false} -> {:error, "timestamp must be a number"}
@@ -116,21 +121,21 @@ defmodule Blxx.Dag do
     end
   end
 
-
   def dets_nodes(detspath) do
     # get all the vertices, vertedges, and edges from the vstore
     :dets.foldl(fn elem, acc -> [elem | acc] end, [], detspath)
     |> Enum.sort_by(fn x -> elem(x, 2) end)
   end
 
-
   def clean_nodes(detspath) do
     # remove all vertices, vertedges, and edges from the vstore
     :dets.delete_all_objects(detspath)
+
     :dets.insert_new(
       detspath,
       {:root, None, Blxx.Util.utc_stamp(), :vertex, %{desc: "root vertex"}}
     )
+
     {:ok, {detspath, expand_graph(:digraph.new(), dets_nodes(detspath))}}
   end
 
@@ -160,10 +165,11 @@ defmodule Blxx.Dag do
           :digraph.del_path(graph, parent, v)
 
         :delsubtree ->
+          nil
           # TODO
       end
     end)
+
     graph
   end
-
 end
--- test/dag_test.exs
+++ test/dag_test.exs
@@ -3,17 +3,107 @@ defmodule TestDag do
   use ExUnit.Case
   doctest Blxx.Dag
 
-  test "open_vstore" do
+  def fileman(
+        name \\ (Blxx.Util.utc_stamp() |> to_string) <> ".dets",
+        dir \\ "/tmp"
+      ) do
     filepath =
-      "~/scratch/" <> (Blxx.Util.utc_stamp() |> to_string()) <> ".dets"
+      Path.join(dir, name)
       |> Path.expand()
       |> to_charlist()
-    IO.inspect filepath
+
     {:ok, {d, g}} = Blxx.Dag.open_store(filepath)
+    {:ok, {d, g}, filepath}
+  end
+
+  test "open_vstore" do
+    {:ok, {d, g}, f} = fileman()
     assert :digraph.vertices(g) == [:root]
     assert :digraph.edges(g) == []
     assert {:ok, _} = Blxx.Dag.clean_nodes(d)
     assert :ok == Blxx.Dag.close_store(d)
+    assert :ok = File.rm!(f)
   end
 
+  test "add_vertex" do
+    {:ok, {d, g}, f} = fileman()
+    {isokay, x} = Blxx.Dag.add_vertex({d, g}, :a)
+    assert isokay == :ok
+    {isokay, x} = Blxx.Dag.add_vertex({d, g}, :b)
+    assert isokay == :ok
+    {isokay, x} = Blxx.Dag.add_vertex({d, g}, :c)
+    assert isokay == :ok
+    {isokay, x} = Blxx.Dag.add_edge({d, g}, :b, :c)
+    assert isokay == :ok
+
+    assert Enum.all?(
+             Enum.map([:a, :b, :c, :root], fn v -> Enum.member?(:digraph.vertices(g), v) end)
+           )
+
+    assert :ok == Blxx.Dag.close_store(d)
+    assert :ok = File.rm!(f)
+  end
+
+  test "make_fx" do
+    {:ok, {d, g}, f} = fileman()
+
+    {isokay, {d, g}} =
+      Blxx.Dag.add_vertex({d, g}, :fx, :root, %{:asset_class => "foreign exchange"})
+
+    assert isokay == :ok
+
+    curr = [
+      :EURPLN,
+      :EURCZK,
+      :USDUSD,
+      :USDGBP,
+      :USDCHF,
+      :USDJPY,
+      :USDSEK,
+      :USDDKK,
+      :USDHUF,
+      :USDRUB,
+      :USDTRY,
+      :USDMXN,
+      :USDZAR,
+      :USDBRL,
+      :USDPLN,
+      :USDCAD,
+      :USDAUD,
+      :USDSGD,
+      :USDHKD,
+      :USDCNY,
+      :USDINR,
+      :USDKRW,
+      :USDIDR,
+      :USDMYR,
+      :USDTHB,
+      :USDPHP,
+      :USDCOP,
+      :USDPEN,
+      :USDCZK,
+      :USDRON,
+      :USDBGN
+    ]
+
+    List.foldl(curr, g, fn v, g ->
+      case Blxx.Dag.add_vertex({d, g}, v, :fx) do
+        {:ok, {_, g}} ->
+          g
+
+        {:error, x} ->
+          IO.puts("error: #{x}")
+          g
+      end
+    end)
+
+    # check if edges are all there
+    edgevs = Enum.map(:digraph.edges(g), fn e -> Blxx.Dag.edge_vertices(g, e) end)
+    assert Enum.all?(Enum.map(curr, fn v -> Enum.member?(edgevs, {:fx, v}) end))
+    # check if vertices are all there
+    assert Enum.all?(Enum.map(curr, fn v -> Enum.member?(:digraph.vertices(g), v) end))
+    Blxx.Dag.close_store(d)
+    assert :ok = File.rm!(f)
+    {:ok, g}
+  end
 end
