9d6fcc175bec989c8d1dd6a6e1306b25b6ac441d
vegabook <thomas@crvm.io>
2024-02-12 23:08:57 +0000

    copy paste ping_client

--- clients/bbg/ping_client.py
+++ clients/bbg/ping_client.py
@@ -1,23 +1,22 @@
 import asyncio
 import websockets
+import ssl
 import time
 
 async def ping_forever():
     uri = "wss://crvm.io"
-    async with websockets.connect(uri) as websocket:
+    async with websockets.connect(uri = uri) as websocket:
         while True:
             start_time = time.time()  # Record the time before sending "ping"
             await websocket.send("ping")
             await websocket.recv()
             end_time = time.time()  # Record the time after receiving "pong"
-            
+
             rtt = end_time - start_time
             print(f"Round-Trip Time: {rtt:.4f} seconds")
-            
+
             # Wait a bit before sending the next ping to avoid overwhelming the server
             await asyncio.sleep(1)
 
 if __name__ == "__main__":
     asyncio.run(ping_forever())
-
-
