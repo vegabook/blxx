c5cd40ffdc1b90a70da8340bf7775803898fcbca
vegabook <thomas@crvm.io>
2024-02-12 20:54:25 +0000

    poing to ip

--- clients/bbg/ping_client.py
+++ clients/bbg/ping_client.py
@@ -3,7 +3,7 @@ import websockets
 import time
 
 async def ping_forever():
-    uri = "ws://suprabonds.com:4002"
+    uri = "ws://80.64.218.175:8080"
     async with websockets.connect(uri) as websocket:
         while True:
             start_time = time.time()  # Record the time before sending "ping"
