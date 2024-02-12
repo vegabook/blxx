769c3ae42e9965355012e909106a740bb7125868
vegabook <thomas@crvm.io>
2024-02-12 23:16:33 +0000

    with file write

--- clients/bbg/ping_client.py
+++ clients/bbg/ping_client.py
@@ -5,18 +5,22 @@ import time
 
 async def ping_forever():
     uri = "wss://crvm.io"
-    async with websockets.connect(uri = uri) as websocket:
-        while True:
-            start_time = time.time()  # Record the time before sending "ping"
-            await websocket.send("ping")
-            await websocket.recv()
-            end_time = time.time()  # Record the time after receiving "pong"
+    # open text file for writing
+    with open("ping_times.txt", "w") as file:
+        async with websockets.connect(uri = uri) as websocket:
+            while True:
+                start_time = time.time()  # Record the time before sending "ping"
+                await websocket.send("ping")
+                await websocket.recv()
+                end_time = time.time()  # Record the time after receiving "pong"
 
-            rtt = end_time - start_time
-            print(f"Round-Trip Time: {rtt:.4f} seconds")
+                rtt = end_time - start_time
+                str1 = f"Round-Trip Time: {rtt:.4f} seconds"
+                print(str1)
+                file.write(str1 + "\n")
 
-            # Wait a bit before sending the next ping to avoid overwhelming the server
-            await asyncio.sleep(1)
+                # Wait a bit before sending the next ping to avoid overwhelming the server
+                await asyncio.sleep(1)
 
 if __name__ == "__main__":
     asyncio.run(ping_forever())
--- clients/bbg/pong_server.py
+++ clients/bbg/pong_server.py
@@ -1,9 +1,11 @@
 import asyncio
 import websockets
+import time
 
 async def echo(websocket):
     async for message in websocket:
         if message == "ping":
+            print("Received ping", time.time())
             await websocket.send("pong")
 
 async def main():
